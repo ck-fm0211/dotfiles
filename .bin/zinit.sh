dbbbcb59be81ef0d9ebad0d12f0c930b973ed20e
ck-fm0211 <17825200+ck-fm0211@users.noreply.github.com>
2024-12-07 19:40:17 +0900

    update

--- .bin/zinit.sh
+++ .bin/zinit.sh
@@ -4,4 +4,4 @@ set -e
 # https://github.com/zdharma-continuum/zinit
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 source ~/.zshrc
-zinit self-update
+# zinit self-update
